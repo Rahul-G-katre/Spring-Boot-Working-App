7dd06b1060e97f60f2862dbe2ecc524fd94874e0
Rahul Katre <rahulkatre10@gmail.com>
2025-05-18 16:09:35 +0530

    My Sql code for Local

--- Crud2/src/main/java/crud2/Crud2/Crud2Application.java
+++ Crud2/src/main/java/crud2/Crud2/Crud2Application.java
@@ -2,7 +2,6 @@ package crud2.Crud2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class Crud2Application {
 
